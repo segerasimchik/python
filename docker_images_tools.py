095dbe2984f8bc9342fa7645033e02162cbbe608
segerasimchik <segerasimchik@gmail.com>
2023-11-14 00:32:17 +0100

    Initial commit

---
+++ docker_images_tools.py
@@ -0,0 +1,69 @@
+import os, subprocess
+from sys import exit
+
+# Error handler:
+def is_docker_exists():
+    try:
+        subprocess.run(["docker", "--version"], check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    
+docker_ready = is_docker_exists()
+
+if docker_ready:
+    print("Docker is installed && available")
+else:
+    print("Docker is not available. Please install Docker to continue")
+    exit(0)
+
+# Customize here:
+
+f = open("file", "r")
+
+tar_name = "default"
+source_repo = "harbor.altezza.org/frisbee/"
+target_repo = "registry.msk.cht/tdm/"
+
+# End of customize field
+
+source_registry_content = []
+target_registry_content = []
+
+for i in f:
+    source_registry_content.append(i.strip('\n'))
+print(source_registry_content)
+
+class Images:
+
+    def __init__(self, source_content, target_content):
+        self.source_content = source_content
+        self.target_content = target_content
+
+    def pull_images(self):
+        for i in self.source_content:
+            os.system(f"docker pull {i}")
+        return print(f"Images were pulled.")
+
+    def tag_images(self):
+        for i in self.source_content:
+            self.target_content.append(i.replace(f'{source_repo}', f'{target_repo}'))
+            for j in self.target_content:
+                os.system(f"docker tag {i} {j}")
+        print("Images were tagged.")
+        return self.target_content
+
+    def save_docker_images(self):
+        images_list = " ".join(self.target_content)
+        print("Start saving images...")
+        os.system(f"docker save {images_list} > {tar_name}.tar")
+        print("Images were saved.")
+        return images_list
+
+# Class declaration:
+docker_object = Images(source_registry_content, target_registry_content)
+
+# Class methods:
+docker_object.pull_images()
+docker_object.tag_images()
+docker_object.save_docker_images()
