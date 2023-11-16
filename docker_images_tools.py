54b188512732c4e338ce792e70db4dd5771ffb14
segerasimchik <segerasimchik@gmail.com>
2023-11-16 23:34:50 +0100

    More interactive script

--- docker_images_tools.py
+++ docker_images_tools.py
@@ -1,29 +1,34 @@
-import os, subprocess
+import os, subprocess, click
 from sys import exit
 
-# Error handler:
+# Error handlers:
 def is_docker_exists():
     try:
         subprocess.run(["docker", "--version"], check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return True
     except subprocess.CalledProcessError:
-        return False
-    
-docker_ready = is_docker_exists()
+        print("Docker is not available. Please install Docker to continue")
+        return exit(1)
 
-if docker_ready:
-    print("Docker is installed && available")
-else:
-    print("Docker is not available. Please install Docker to continue")
-    exit(0)
+def is_file_exists():
+    try:
+        open(f"{filename}", "r")
+        return True
+    except OSError:
+        print("File not found")
+        return exit(1)
+# End of error handlers
 
-# Customize here:
+is_docker_exists()
+filename = input("Please provide filename with docker images list: ")
+is_file_exists()
 
-f = open("file", "r") # Filename with images list (need to be in current dir). Replace "file" with your filename
+# Customize field:
+f = open(f"{filename}", "r") # Filename with images list (need to be in current dir). Replace "file" with your filename
 
 tar_name = input("Enter result archive name (without .tar): ")
-source_repo = "harbor.altezza.org/frisbee/"
-target_repo = "registry.msk.cht/tdm/"
+source_repo = input("Spicify source registry with project (for exmpl 'harbor.altezza.org/frisbee'): ")
+target_repo = input("Spicify source registry with project (for exmpl 'registry.msk.cht/tdm'): ")
 
 # End of customize field
 
@@ -49,17 +54,21 @@ class Images:
     def tag_images(self):
         for i in self.source_content:
             self.target_content.append(i.replace(f'{source_repo}', f'{target_repo}'))
-        for i in range(len(self.source_content)):
-            print(f"{self.source_content[i]} {self.target_content[i]}")
-            os.system(f"docker tag {self.source_content[i]} {self.target_content[i]}")
-        print("Images were tagged.")
+        print(self.target_content)
+        if click.confirm(f"Are you agree with the result?", default=True):
+            for i in range(len(self.source_content)):
+                print(f"{self.source_content[i]} {self.target_content[i]}")
+                os.system(f"docker tag {self.source_content[i]} {self.target_content[i]}")
+            print("Images were tagged.")
+        else:
+            exit(1)
         return self.target_content
 
     def save_docker_images(self):
         images_list = " ".join(self.target_content)
         print("Start saving images...")
         os.system(f"docker save {images_list} > {tar_name}.tar")
-        print("Images were saved.")
+        print("Images were saved. Please check it.")
         return images_list
 
 # Object declaration:
