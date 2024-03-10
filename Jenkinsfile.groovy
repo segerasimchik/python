ecf3532f7975bfa3812ccaf598cbb6d84114dbbc
segerasimchik <segerasimchik@gmail.com>
2024-03-10 21:32:43 +0100

    changeset added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,20 @@
 node {
+
     checkout scm
     stage('Hello') {
         echo "Hello world!"
     }
 
+    stage('Check for Changes') {
+        script {
+            def changes = checkout([$class: 'GitSCM']).poll().changeset
+            if (changes.isEmpty()) {
+                echo "No changes detected in the repository"
+            } else {
+                echo "Changes detected in the repository"
+            }
+        }
+    }
 }
 
 
