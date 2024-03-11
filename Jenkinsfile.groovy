0b739b1aa5975c969813ad7778bc65883f4f7f71
Sergey Gerasimchik <segerasimchik@gmail.com>
2024-03-11 12:26:26 +0100

    dry_run added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,27 @@
+properties([
+    parameters([
+        string(defaultValue: 'master', description: 'Version  branch/commit/tag', name: 'branch'),
+        booleanParam(defaultValue: true, description: 'Execute job in dry-run mode, this mean that all deployments and notifications are skipped', name: 'dry_run'),
+    ])
+])
+
 node {
+
+    stage('Dry run') {
+        if (dry_run.toBoolean()) {
+            currentBuild.result == "SUCCESS"
+        } else {
+            echo "Continue build.."
+        }
+    }
 
     stage('Hello') {
         echo "Hello world!"
-        checkout scm
+        def checkout = checkout([$class: "GitSCM",
+            branches: [[name: branch]],
+            extensions: [[$class: 'CleanBeforeCheckout']],
+            userRemoteConfigs: [[credentialsId: "jenkins_github_ssh", url: "git@github.com:Playtika/bingoblitz-versions.git"]]
+        ])
     }
 
     stage('Check for Changes') {
