f0afdbaf9e6ecf5859ea048a7df3937932d66edf
Sergey Gerasimchik <segerasimchik@gmail.com>
2024-03-11 12:35:27 +0100

    fix for quotes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ node {
 
     stage('Dry run') {
         if (dry_run.toBoolean()) {
-            currentBuild.result == "SUCCESS"
+            currentBuild.result = 'SUCCESS'
         } else {
             echo "Continue build.."
         }
